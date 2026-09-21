c5d17854955680e91813b6d0523966384a77468e
luis.hoyos <luis.hoyos1@ct.uneatlantico.es>
2026-09-21 11:13:58 +0200

    Implementa salidas simples de Console

--- entregas/src/reto 001/Console.java
+++ entregas/src/reto 001/Console.java
@@ -1,13 +1,17 @@
 public class Console {
     public void write(String texto) {
+        System.out.print(texto);
     }
 
     public void writeln(String texto) {
+        System.out.println(texto);
     }
 
     public void writeln(int numero) {
+        System.out.println(numero);
     }
 
     public void writeln() {
+        System.out.println();
     }
 }
