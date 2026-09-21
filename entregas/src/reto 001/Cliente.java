dc038e9046e305d16d4ef0a904d3babb0b8e1147
luis.hoyos <luis.hoyos1@ct.uneatlantico.es>
2026-09-21 11:03:49 +0200

    Implementa los métodos añadirCliente y quitarCliente en la clase Cliente para gestionar la lista de clientes.

--- entregas/src/reto 001/Cliente.java
+++ entregas/src/reto 001/Cliente.java
@@ -1,5 +1,22 @@
 public class Cliente {
 
-    public void añadirCliente(Cliente cliente) {}
-    
+
+public void añadirCliente(Cliente cliente) {
+    clientes[tamaño] = cliente;
+    tamaño = tamaño + 1;
+}
+
+
+public Cliente quitarCliente() {
+    Cliente cliente = clientes[0];
+
+    for (int i = 0; i < tamaño - 1; i++) {
+        clientes[i] = clientes[i + 1];
+    }
+
+    clientes[tamaño - 1] = null;
+    tamaño = tamaño - 1;
+
+    return cliente;
+}
 }
