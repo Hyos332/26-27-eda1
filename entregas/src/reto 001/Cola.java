ac538b91623c8e4cbca189f594103403c45eecd6
luis.hoyos <luis.hoyos1@ct.uneatlantico.es>
2026-09-21 11:11:13 +0200

    Define vista publica basica de Cola

--- entregas/src/reto 001/Cola.java
+++ entregas/src/reto 001/Cola.java
@@ -1,5 +1,11 @@
-package entregas.src.reto 001;
-
 public class Cola {
-    
+    public Cola() {
+    }
+
+    public void anadirCliente(Cliente cliente) {
+    }
+
+    public Cliente quitarCliente() {
+        return null;
+    }
 }
